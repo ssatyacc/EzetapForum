a3fb995003d7287843a9d1691f97700277f4ef96
Satya Chaitanya <satya.chaitanya@zimmber.com>
2016-09-29 21:08:53 +0530

    Remove minValue and maxValue Functionality

--- app/src/main/java/ezetap/satya/ezetapform/Screen1.java
+++ app/src/main/java/ezetap/satya/ezetapform/Screen1.java
@@ -2,10 +2,8 @@ package ezetap.satya.ezetapform;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.text.InputFilter;
 import android.text.InputType;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -82,8 +80,6 @@ public class Screen1 extends AppCompatActivity {
                     String textboxName = item.getString("name");
                     final String textboxInputType = item.getString("type");
                     int textboxMaxLength = item.optInt("maxlength", -1);
-                    final int textboxMinValue = item.optInt("minValue", Integer.MIN_VALUE);
-                    final int textboxMaxValue = item.optInt("maxValue", Integer.MAX_VALUE);
 
                     TextView textboxTextView = new TextView(this);
                     textboxTextView.setLayoutParams(new LinearLayout.LayoutParams(
@@ -114,45 +110,6 @@ public class Screen1 extends AppCompatActivity {
                         filters.add(new InputFilter.LengthFilter(textboxMaxLength));
                     }
 
-                    if (textboxEditText.getInputType() == InputType.TYPE_CLASS_NUMBER) {
-                        textboxEditText.addTextChangedListener(new TextWatcher() {
-                            @Override
-                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                                // Do Nothing
-                            }
-
-                            @Override
-                            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                                // Do Nothing
-                            }
-
-                            @Override
-                            public void afterTextChanged(Editable s) {
-                                String replaceString;
-                                String inputNumber = textboxEditText.getText().toString();
-
-                                if (!isInt(inputNumber) || Integer.valueOf(inputNumber) > textboxMaxValue) {
-                                    replaceString = "" + textboxMaxValue;
-                                    textboxEditText.setText(replaceString);
-                                    textboxEditText.setSelection(replaceString.length());
-                                } else if (Integer.valueOf(inputNumber) < textboxMinValue) {
-                                    replaceString = "" + textboxMinValue;
-                                    textboxEditText.setText(replaceString);
-                                    textboxEditText.setSelection(replaceString.length());
-                                }
-                            }
-
-                            private boolean isInt(String s) {
-                                try {
-                                    Integer.valueOf(s);
-                                    return true;
-                                } catch (Exception e) {
-                                    return false;
-                                }
-                            }
-                        });
-                    }
-
                     InputFilter[] inputFilters = new InputFilter[filters.size()];
                     if (filters.size() > 0) {
                         textboxEditText.setFilters(filters.toArray(inputFilters));
@@ -165,7 +122,7 @@ public class Screen1 extends AppCompatActivity {
 
                     break;
                 case "dropdown":
-                    String dropdownHint = item.getString("name");
+                    String dropdownName = item.getString("name");
                     JSONArray valuesArray = item.getJSONArray("values");
 
                     String[] dropdownValues = new String[valuesArray.length()];
@@ -177,7 +134,7 @@ public class Screen1 extends AppCompatActivity {
                     dropdownTextView.setLayoutParams(new LinearLayout.LayoutParams(
                             LinearLayout.LayoutParams.WRAP_CONTENT,
                             LinearLayout.LayoutParams.WRAP_CONTENT));
-                    dropdownTextView.setText(dropdownHint);
+                    dropdownTextView.setText(dropdownName);
 
                     Spinner dropdownSpinner = new Spinner(this);
                     ArrayAdapter<String> dropdownAdapter = new ArrayAdapter<String>(this,
